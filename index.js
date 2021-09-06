f74d7ee3633af0319f2446fbedec2af317179a16
Karen Okonkwo <karenokonkwo29@gmail.com>
2021-09-06 19:46:58 +0100

    Update fallback port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { getRandomSentence, getResponseInterval } = require("./utils");
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 const app = express();
 const server = app.listen(PORT, () => console.log("Server running..."));
 
